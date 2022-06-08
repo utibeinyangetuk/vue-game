01be6863e20dee4dbfae91a95c52c7aea949b512
utibe inyangetuk <inyangutibz@gmail.com>
2022-06-08 17:11:31 +0100

    All functions working

--- app.js
+++ app.js
@@ -9,26 +9,34 @@ const app = Vue.createApp({
 			user: 100,
 			monster: 100,
 			currentRound: 0,
+			winner: null,
+			battleLog: [],
 		};
 	},
 	computed: {
 		damage() {
-			if (this.monster < 100) {
-				return { width: `${this.monster}%`, "background-color": "gold" };
+			if (this.monster < 0) {
+				this.monster = 0;
+				return { width: "0%" };
+			} else if (this.monster < 100) {
+				return { width: `${this.monster}%`, "background-color": "yellow" };
 			}
 		},
 		damage2() {
-			if (this.monster < 50) {
+			if (this.monster <= 50) {
 				return { "background-color": "red" };
 			}
 		},
 		damage3() {
-			if (this.user < 100) {
-				return { width: `${this.user}%`, "background-color": "gold" };
+			if (this.user < 0) {
+				this.user = 0;
+				return { width: "0%" };
+			} else if (this.user < 100) {
+				return { width: `${this.user}%`, "background-color": "yellow" };
 			}
 		},
 		damage4() {
-			if (this.user < 50) {
+			if (this.user <= 50) {
 				return { "background-color": "red" };
 			}
 		},
@@ -64,6 +72,37 @@ const app = Vue.createApp({
 				this.user = heal;
 			}
 			this.attackPlayer();
+		},
+		restart() {
+			this.user = 100;
+			this.monster = 100;
+			this.winner = null;
+			this.currentRound = 0;
+			this.log = [];
+		},
+		surrender() {
+			this.user = 0;
+			this.winner = "monster";
+		},
+	},
+	watch: {
+		user(value) {
+			if (value <= 0 && this.monster <= 0) {
+				// draw
+				this.winner = "draw";
+			} else if (value <= 0) {
+				// player lost
+				this.winner = "monster";
+			}
+		},
+		monster(value) {
+			if (value <= 0 && this.user <= 0) {
+				// draw
+				this.winner = "draw";
+			} else if (value <= 0) {
+				// monster lost
+				this.winner = "user";
+			}
 		},
 	},
 });
